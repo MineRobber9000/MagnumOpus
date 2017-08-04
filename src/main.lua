184c191bacb02bbc0c1fefbf9033b0df8e2032cc
MineRobber___T <MineRobber9000@users.noreply.github.com>
2017-08-04 07:07:47 -0400

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -1,3 +1,24 @@
--- Main functionality (not much atm)
+-- Main functionality (basic shell)
 
-print("Testing 123")
+local function setColor(c)
+	if term.isColor() then term.setTextColor(c) end
+end
+
+term.clear()
+os.version = function() return "MagnumOpus v0.1" end
+setColor(colors.yellow)
+print(os.version())
+
+local tCommandHistory = {}
+
+while true do
+	setColor(colors.yellow)
+	write(shell.dir().."> ")
+    local sLine
+    if settings.get( "shell.autocomplete" ) then
+		sLine = read( nil, tCommandHistory, shell.complete )
+    else
+		sLine = read( nil, tCommandHistory )
+    end
+	shell.run(sLine)
+end
